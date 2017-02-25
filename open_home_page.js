bff93a2f24b01e7e2346bbe56fb329f4b8bb5796
Natalie Poda <natalie.poda@speedandfunction.com>
2017-02-25 12:41:12 -0500

    added first test

---
+++ open_home_page.js
@@ -0,0 +1,32 @@
+
+var webdriver = require('selenium-webdriver'),
+    chrome = require('selenium-webdriver/chrome'),
+    By = webdriver.By,
+    until = webdriver.until,
+    test = require('selenium-webdriver/testing');
+
+test.describe('Google Search', function() {
+    var driver;
+
+    test.before(function() {
+        var options = new chrome.Options();
+        options.addArguments(["start-fullscreen"]);
+
+        driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(options)
+            .build();
+        driver.getCapabilities().then(function(caps) {
+            console.log(caps);
+        });
+    });
+
+    test.it('should open Home page', function() {
+        driver.get('https://www.google.com/');
+        driver.wait(until.titleIs('Google'), 1000);
+    });
+
+    test.after(function() {
+        driver.quit();
+    });
+});
